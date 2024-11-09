c7b3e7957536ae39031efdcdd084ebf2bedd9baf
omaromarsalhi <omar.salhi.job@gmail.com>
2024-11-09 15:30:26 +0100

    text 2 sql is completed

--- sql_db_search/text_to_sql.py
+++ sql_db_search/text_to_sql.py
@@ -1,28 +1,46 @@
+import configparser
 from llama_index.core import SQLDatabase, Document, VectorStoreIndex, Settings, QueryBundle
 from llama_index.core.indices.struct_store import SQLTableRetrieverQueryEngine
 from llama_index.core.objects import SQLTableNodeMapping, SQLTableSchema, ObjectIndex
 from llama_index.embeddings.gemini import GeminiEmbedding
 from llama_index.llms.gemini import Gemini
+from openai import api_key
 from sqlalchemy import create_engine, inspect
 from sql_db_search.prompt import custom_prompt, response_prompt
 from vars import get_gemini_api_key
 
 
-Settings.llm = Gemini(get_gemini_api_key())
-Settings.embed_model = GeminiEmbedding(api_key=get_gemini_api_key())
-# Set up the engine and inspector
-engine = create_engine('mysql+pymysql://root:@localhost:3306/rag_db')
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+db_host = config['DATABASE']['host']
+db_user = config['DATABASE']['user']
+db_password = config['DATABASE']['password']
+db_port = config['DATABASE']['port']
+db_name = config['DATABASE']['db_name']
+gemini_key = config['API']['gemini_key']
+
+
+Settings.llm = Gemini(gemini_key)
+Settings.embed_model = GeminiEmbedding(api_key=gemini_key)
+
+
+engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 inspector = inspect(engine)
 
-# Extract table and column information
+
+
 tables = inspector.get_table_names()
 
+
 sql_database = SQLDatabase(engine)
+
 
 table_node_mapping = SQLTableNodeMapping(sql_database)
 table_schema_objs = []
 for table in tables:
     table_schema_objs.append((SQLTableSchema(table_name=table)))
+
 
 obj_index = ObjectIndex.from_objects(
     table_schema_objs,
@@ -30,7 +48,7 @@ obj_index = ObjectIndex.from_objects(
     VectorStoreIndex
 )
 
-# Initialize the SQL database and query engine
+
 query_engine = SQLTableRetrieverQueryEngine(
     sql_database=sql_database,
     table_retriever=obj_index.as_retriever(similarity_top_k=3),
