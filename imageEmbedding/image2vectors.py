db70f02ffb21c42ba236156f00e12fa94ca69209
omaromarsalhi <omar.salhi.job@gmail.com>
2024-11-13 21:32:57 +0100

    saving the images vectors in pinecone

--- imageEmbedding/image2vectors.py
+++ imageEmbedding/image2vectors.py
@@ -1,24 +1,26 @@
 import configparser
-
-from llama_index.core import download_loader, Settings, StorageContext, VectorStoreIndex
+from llama_index.core import Settings, StorageContext,SimpleDirectoryReader
+from llama_index.core.indices import MultiModalVectorStoreIndex
+from llama_index.core.indices.vector_store import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.embeddings.gemini import GeminiEmbedding
 from llama_index.llms.gemini import Gemini
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
+
 
 config = configparser.ConfigParser()
 config.read("../config.ini")
 GOOGLE_API_KEY = config["API"]["gemini_key"]
 PINECONE_API_KEY = config["API"]["pinecone_key"]
 
-DATA_URL = "https://www.gettingstarted.ai/how-to-use-gemini-pro-api-llamaindex-pinecone-index-to-build-rag-app"
+DATA_PATH= "fruit_images/"
 
-llm = Gemini(GOOGLE_API_KEY)
-embed_model = GeminiEmbedding(api_key=GOOGLE_API_KEY, model_name="models/embedding-001")
 
-Settings.llm = llm
-Settings.embed_model = embed_model
+Settings.llm = Gemini(GOOGLE_API_KEY)
+Settings.embed_model = GeminiEmbedding(api_key=GOOGLE_API_KEY)
 Settings.chunk_size = 512
+
 
 pinecone_client = Pinecone(PINECONE_API_KEY)
 index_name = "images"
@@ -32,12 +34,6 @@ if index_name not in pinecone_client.list_indexes().names():
 
 pinecone_index = pinecone_client.Index(index_name)
 
-BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
-
-loader = BeautifulSoupWebReader()
-documents = loader.load_data(urls=[DATA_URL])
-print(documents)
-
 # Create a PineconeVectorStore using the specified pinecone_index
 vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
 
@@ -46,14 +42,16 @@ storage_context = StorageContext.from_defaults(
     vector_store=vector_store
 )
 
-# Use the chunks of documents and the storage_context to create the index
-index = VectorStoreIndex.from_documents(
+documents = SimpleDirectoryReader(DATA_PATH).load_data()
+
+index = MultiModalVectorStoreIndex.from_documents(
     documents,
-    storage_context=storage_context
+    storage_context=storage_context,
 )
 
-query_engine = index.as_query_engine()
+retriever = VectorIndexRetriever(index=index, similarity_top_k=1)
+query_engine = RetrieverQueryEngine(retriever=retriever)
 
-gemini_response = query_engine.query("What does the author think about LlamaIndex?")
-
-print(gemini_response)
+# Run a sample query
+response = query_engine.query("Is there a banana in there?")
+print("Query response:", response)
