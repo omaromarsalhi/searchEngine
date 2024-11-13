f81d21910e1ef92c6b51037d8fd4b6ef0918f978
omaromarsalhi <omar.salhi.job@gmail.com>
2024-11-13 19:28:41 +0100

    working on image QA

--- imageEmbedding/retriever.py
+++ imageEmbedding/retriever.py
@@ -1,3 +1,4 @@
+import configparser
 import os
 from llama_index.core.indices.vector_store import VectorIndexRetriever
 from llama_index.core.query_engine import RetrieverQueryEngine
@@ -8,9 +9,13 @@ from pinecone import Pinecone
 from llama_index.core import SimpleDirectoryReader, StorageContext, VectorStoreIndex
 from llama_index.core.settings import Settings
 
-GOOGLE_API_KEY = ""
-os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
-PINECONE_API_KEY = ""
+
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+GOOGLE_API_KEY = config["API"]["gemini_key"]
+PINECONE_API_KEY = config["API"]["pinecone_key"]
+
 
 Settings.embed_model = GeminiEmbedding(
     model_name="models/embedding-001", api_key=GOOGLE_API_KEY
@@ -23,16 +28,12 @@ pc = Pinecone(api_key=PINECONE_API_KEY)
 
 pinecone_index = pc.Index("googleimages")
 
-# Initialize the PineconeVectorStore with the Pinecone index
 vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
 
-# Create a StorageContext with the PineconeVectorStore
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-# Load your documents
 documents = SimpleDirectoryReader("google_images_index").load_data()
 
-# Create the VectorStoreIndex with the documents and the StorageContext
 index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 
 retriever = VectorIndexRetriever(index=index, similarity_top_k=1)
@@ -44,7 +45,4 @@ while (prompt := input("Enter a prompt (q to quit): ")) != "q":
         response = query_engine.query(prompt)
         print(response)
     except ValueError as e:
-        if str(e) == "Reached max iterations.":
-            print("Maximum iterations reached. Restarting the process...")
-        else:
-            raise
+        raise
